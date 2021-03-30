8b705a06efdf194a3dea1b51bc00301b10e8a446
wang ya xuan <wangxuan@wangdeMacBook-Air.local>
2021-03-31 06:31:58 +0800

    解除安裝沒有用到的插件順便更新 README

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import axios from "axios";
-import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
-Vue.use(VueAxios, axios);
-axios.defaults.withCredentials = true;
 
 Vue.filter("currency", function (n) {
   var intPart = Number(n).toFixed(0);
