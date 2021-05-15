469e9155ad0fa7cdefeba02a103a474b42d83704
wang ya xuan <wangxuan@wangdeMacBook-Air.local>
2021-05-15 16:29:47 +0800

    fix img

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -10,7 +10,11 @@
           @click="showAlbum(currentRoom.imageUrl[i])"
         >
           <router-link to="/" class="preBtn">
-            <img src="/img/icons/previous.svg" class="preBtn-img" alt="" />
+            <img
+              :src="require(`../assets/img/icons/previous.svg`)"
+              class="preBtn-img"
+              alt=""
+            />
             查看其它房型
           </router-link>
 
@@ -62,11 +66,16 @@
         >
           <img
             v-if="item.state"
-            src="/img/icons/ok.svg"
+            :src="require(`../assets/img/icons/ok.svg`)"
             alt=""
             class="positionIcon"
           />
-          <img v-else src="/img/icons/cancel.svg" alt="" class="positionIcon" />
+          <img
+            v-else
+            :src="require(`../assets/img/icons/cancel.svg`)"
+            alt=""
+            class="positionIcon"
+          />
           <img
             class="room-icon-img"
             :src="`./img/icons/${item.name}.svg`"
@@ -96,7 +105,7 @@
 
     <div v-if="imgUrl" class="album" @click="imgUrl = ''">
       <img
-        src="/img/icons/album-prev.svg"
+        :src="require(`../assets/img/icons/album-prev.svg`)"
         class="album-btn"
         @click.stop="lessImgUrl()"
         alt=""
@@ -107,7 +116,7 @@
         class="album-img"
       ></div>
       <img
-        src="/img/icons/album-next.svg"
+        :src="require(`../assets/img/icons/album-next.svg`)"
         class="album-btn"
         @click.stop="addImgUrl()"
         alt=""
